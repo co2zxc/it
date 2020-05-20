03056c98cb93de12d525959a6572ddffbc12eb53
unknown <co2zxc@gmail.com>
2020-05-20 14:45:32 +0800

    0520

---
+++ Windows 10 移除APP-使用PowerShell執行/RemoveApp.ps1
@@ -0,0 +1,77 @@
+#天氣
+get-appxpackage *BingWeather* | remove-appxpackage
+#取得協助
+get-appxpackage *GetHelp* | remove-appxpackage
+#Microsoft 提示
+get-appxpackage *Getstarted* | remove-appxpackage
+#HEIF 擴充功能
+get-appxpackage *HEIF* | remove-appxpackage
+#Microsoft 訊息中心
+get-appxpackage *Messaging* | remove-appxpackage
+#混合實境檢視器
+get-appxpackage *3DViewer* | remove-appxpackage
+#我的 Office
+get-appxpackage *OfficeHub* | remove-appxpackage
+#Microsoft Solitaire Collection 接龍
+get-appxpackage *Solitaire* | remove-appxpackage
+#自黏便箋
+#get-appxpackage *StickyNotes* | remove-appxpackage
+#混合實境入口
+get-appxpackage *Mixed* | remove-appxpackage
+#Paint 3D
+get-appxpackage *MSPaint* | remove-appxpackage
+#OneNote
+get-appxpackage *OneNote* | remove-appxpackage
+#付費的Wifi & 行動數據連線
+get-appxpackage *OneConnect* | remove-appxpackage
+#聯絡人
+#get-appxpackage Microsoft.People | remove-appxpackage
+#Print 3D
+get-appxpackage *Print3D* | remove-appxpackage
+#剪取與繪圖
+#get-appxpackage *ScreenSketch* | remove-appxpackage
+#Get Skype
+get-appxpackage *SkypeApp* | remove-appxpackage
+#市集購買應用程式
+get-appxpackage *StorePurchaseApp* | remove-appxpackage
+#Microsoft Pay
+get-appxpackage *Wallet* | remove-appxpackage
+#相片
+get-appxpackage *Photos* | remove-appxpackage
+#鬧鐘 & 時鐘
+get-appxpackage *Alarms* | remove-appxpackage
+#小算盤
+#get-appxpackage *Calculator* | remove-appxpackage
+#相機
+get-appxpackage *Camera* | remove-appxpackage
+#郵件與行事曆
+get-appxpackage *Communicationsapps* | remove-appxpackage
+#意見反應中樞
+get-appxpackage *Feedback* | remove-appxpackage
+#地圖
+get-appxpackage *Maps* | remove-appxpackage
+#語音錄音機
+get-appxpackage *SoundRecorder* | remove-appxpackage
+#你的手機
+get-appxpackage *YourPhone* | remove-appxpackage
+#Groove Music
+get-appxpackage *ZuneMusic* | remove-appxpackage
+#電影與電視
+get-appxpackage *ZuneVideo* | remove-appxpackage
+
+#Xbox.TCUI
+get-appxpackage *Xbox.TCUI* | remove-appxpackage
+#Xbox.XboxApp
+get-appxpackage *XboxApp* | remove-appxpackage
+#Xbox 遊戲列
+get-appxpackage *XboxGameOverlay* | remove-appxpackage
+#Xbox 遊戲重疊
+get-appxpackage *XboxGamingOverlay* | remove-appxpackage
+#Xbox 身分識別提供者
+get-appxpackage *XboxIdentityProvider* | remove-appxpackage
+#Xbox Speech To Text Overlay
+get-appxpackage *XboxSpeechToTextOverlay* | remove-appxpackage
+
+#1809之前
+#3D Builder
+get-appxpackage *3DBuilder* | remove-appxpackage
