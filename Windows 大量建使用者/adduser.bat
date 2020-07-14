22cce642d232b7c5fff21929a8de9780e69340aa
co2zxc <co2zxc@gmail.com>
2020-07-14 10:01:28 +0800

    Add files via upload

---
+++ Windows 大量建使用者/adduser.bat
@@ -0,0 +1,3 @@
+﻿FOR /L %i IN (1,1,9) DO net user user0%%i P@sswOrd /add
+FOR /L %i IN (10,1,50) DO net user user%%i P@sswOrd /add
+
---
+++ Windows 大量建使用者/adduser.ps1
@@ -0,0 +1,31 @@
+﻿#Set-ExecutionPolicy RemoteSigned
+if (!$args) { 
+    Write-Host "需要 CSV 檔案!"
+} 
+
+ELSE
+{
+
+#載入 CSV 檔
+$accountdata=Import-CSV $args
+  foreach ($element in $accountdata){ 
+    
+    #USER帳號 
+    $name=$element.name 
+
+    #帳號
+    $userPrincipalName=$name 
+    #密碼處理,要編碼
+    $password=$element.password 
+    $pass = ConvertTo-SecureString $password -AsPlainText -Force
+    #中文名
+    $description=$element.description 
+    $displayName=$element.displayName 
+    #群組
+    $group=$element.group
+
+    New-ADUser -Name $name -AccountPassword $pass  -Enabled 1 -ScriptPath "login.vbs" -SamAccountName $name -UserPrincipalName $userPrincipalName -OtherAttributes @{'description'=$description;'displayName'=$description; }
+    #把帳號加入群組
+    Add-ADGroupMember -Identity $group -Members $name
+  } #end for
+} # end if else
